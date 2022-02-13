cb4f8992edd493727c1a92e97a989766aa9c6582
ArtemChernov33 <94698583+ArtemChernov33@users.noreply.github.com>
2022-02-13 18:11:01 +0300

    Add files via upload
    Мои последние исправления

--- kursovay.py
+++ kursovay.py
@@ -5,85 +5,107 @@ from tqdm import tqdm
 from pprint import pprint
 
 
-#URL_id = 'https://api.vk.com/method/users.get'
 
-with open('token.txt', 'r') as file_object:
-    token = file_object.read().strip()
 
-with open('tokenYD.txt', 'r') as file_object:
-    tokenYD = file_object.read().strip()
+class VK:
+    # with open('token.txt', 'r') as file_object:
+    #     token = file_object.read().strip()
+    #
+    # with open('tokenYD.txt', 'r') as file_object:
+    #     tokenYD = file_object.read().strip()
 
-def get_lagest(size_dict):
-    if size_dict['width'] >= size_dict['height']:
-        return size_dict['width']
-    else:
-        return size_dict['height']
+    def __init__(self, user_ids, count):
+        self.token = ''
+        self.user_ids = []
+        self.foto = {}
+        self.tokenYD = ''
+        self.path = ''
+    def get_lagest(self, size_dict):
+        if size_dict['width'] >= size_dict['height']:
+            return size_dict['width']
+        else:
+            return size_dict['height']
 
-def get_photo(user_name, count):
-    URL_photo = 'https://api.vk.com/method/photos.get'
-    params = {
-        'owner_id': user_name,
-        'access_token': token,
-        'album_id': 'profile',
-        'extended': 'likes',
-        'photo_sizes': '1',
-        'v': '5.131',
-        'count': count
-    }
-    foto = {}
-    res = requests.get(URL_photo, params=params)
-    res_photo = res.json()['response']['items']
-    info_photo = []
-    for photo in res_photo:
-        date = photo['date']
-        likes = photo['likes']
-        file_name = likes.get('count')
-        sizes = photo['sizes']
-        max_size_url = max(sizes, key=get_lagest)['url']
-        max_size_photo = max(sizes, key=get_lagest)['type']
-        time.sleep(0.1)
-        foto[max_size_url] = (f'{file_name}.jpg', date)
-        info_photo.append({'file_name': f'{file_name}.jpg', 'size': max_size_photo})
-
-    """Запись информации по фотографиям в файл json"""
-    with open('info.json', 'w') as outfile:
-        outfile.write(f'{info_photo}\n')
-    return foto
-q = get_photo('2526736', '3')
-#pprint(q)
-
-def get_headers():
-    return {
-        'Content-Type': 'application/json',
-        'Authorization': 'OAuth {}'.format(tokenYD)
-    }
-
-def upload_file(path):
-    url = 'https://cloud-api.yandex.net/v1/disk/resources/'
-    url_z = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {tokenYD}'}
-    download_YD = []
-    c = 0
-    """Создание папки.  path: Путь к создаваемой папке."""
-    create = requests.put(f'{url}?path={path}', headers=headers).json()
-    folder = create.get('method')
-    if folder == 'GET':
-        """Сохраняю фотографии в указанную папку. """
-        for k, v in q.items():
-            name = v[0]
+    def get_photo(self):
+        URL_user = 'https://api.vk.com/method/users.get'
+        URL_photo = 'https://api.vk.com/method/photos.get'
+        params = {
+            'user_ids': self.user_ids,
+            'access_token': self.token,
+            'v': '5.131',
+        }
+        res = requests.get(URL_user, params=params)
+        items = res.json()['response']
+        for id in items:
+            id_user = id['id']
             params = {
-            "path": f'/{path}/{name}',
-            "url": k,
-            "overwrite": "true",
+                'owner_id': id_user,
+                'access_token': self.token,
+                'album_id': 'profile',
+                'extended': 'likes',
+                'photo_sizes': '1',
+                'v': '5.131',
+                'count': count
             }
-            c += 1
-            download_YD.append(c)
-            for i in tqdm(download_YD):
-                time.sleep(0.5)
-            res = requests.post(url=url_z, params=params, headers=headers).json()
-    else:
-        print('Такая папка существует, создайте другую папку')
+        res = requests.get(URL_photo, params=params)
+        res_photo = res.json()['response']['items']
+        info_photo = []
+        for photo in res_photo:
+            date = photo['date']
+            likes = photo['likes']
+            file_name = likes.get('count')
+            sizes = photo['sizes']
+            max_size_url = max(sizes, key=self.get_lagest)['url']
+            max_size_photo = max(sizes, key=self.get_lagest)['type']
+            time.sleep(0.1)
+            self.foto[max_size_url] = (f'{file_name}.jpg', date)
+            # info_photo.append({'file_name': f'{file_name}.jpg', 'size': max_size_photo})
+
+        """Запись информации по фотографиям в файл json"""
+        with open('info.json', 'w') as outfile:
+            outfile.write(f'{info_photo}\n')
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.tokenYD)
+        }
+
+    def upload_file(self, path):
+        f = vk.foto
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/'
+        url_z = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json',
+                   'Authorization': f'OAuth {self.tokenYD}'}
+        download_YD = []
+        c = 0
+        """Создание папки.  path: Путь к создаваемой папке."""
+        create = requests.put(f'{url}?path={path}', headers=headers).json()
+        folder = create.get('method')
+        if folder == 'GET':
+            """Сохраняю фотографии в указанную папку. """
+            for k, v in f.items():
+                name = v[0]
+                pprint(name)
+                params = {
+                    "path": f'/{path}/{name}',
+                    "url": k,
+                    "overwrite": "true",
+                }
+                c += 1
+                download_YD.append(c)
+                for i in tqdm(download_YD):
+                    time.sleep(0.5)
+                res = requests.post(url=url_z, params=params, headers=headers).json()
+        else:
+            print('Такая папка существует, создайте другую папку')
 
 if __name__ == '__main__':
-    get_photo('2526736', '3')
-    upload_file('77')
+    # VK.token = ''
+    # VK.tokenYD = ''
+    id_vk_user = input('Введите id пользователя: ')
+    count = input('Введите кол-во фото для загрузки: ')
+    path = input('Введите адрес для загрузки фотографий: ')
+    vk = VK(id_vk_user, count)
+    vk.get_photo()
+    vk.upload_file(path)
